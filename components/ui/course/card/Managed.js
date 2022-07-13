b04ca984df050df937ef8f656b0e7a832f4097e7
Nirmalya Nayak <n.nayak263@gmail.com>
2022-07-13 20:35:57 +0530

    implemented serch and filter functionality

--- components/ui/course/card/Managed.js
+++ components/ui/course/card/Managed.js
@@ -9,24 +9,30 @@ const Item = ({ title, value, className }) => {
   )
 }
 
-export default function ManagedCourseCard({ children, course }) {
+export default function ManagedCourseCard({
+  children,
+  course,
+  isSearched = false,
+}) {
   return (
-    <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div className="border-t border-gray-200">
-        {Object.keys(course).map((key, index) => (
-          <Item
-            key={key}
-            className={`${index % 2 ? 'bg-gray-50' : 'bg-white'}`}
-            title={key[0].toUpperCase() + key.slice(1)}
-            value={
-              key === 'state'
-                ? course[key][0].toUpperCase() + course[key].slice(1)
-                : course[key]
-            }
-          />
-        ))}
-        <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
-      </div>
+    <div
+      className={`${
+        isSearched ? 'border-indigo-600' : 'bg-gray-200'
+      } bg-white border shadow overflow-hidden sm:rounded-lg mb-3`}
+    >
+      {Object.keys(course).map((key, index) => (
+        <Item
+          key={key}
+          className={`${index % 2 ? 'bg-gray-50' : 'bg-white'}`}
+          title={key[0].toUpperCase() + key.slice(1)}
+          value={
+            key === 'state'
+              ? course[key][0].toUpperCase() + course[key].slice(1)
+              : course[key]
+          }
+        />
+      ))}
+      <div className="bg-white px-4 py-5 sm:px-6">{children}</div>
     </div>
   )
 }
--- components/ui/course/filter/index.js
+++ components/ui/course/filter/index.js
@@ -1,26 +1,40 @@
 import { Button } from '@components/ui/common'
+import { useState } from 'react'
 
-export default function CourseFilter() {
+const OPTIONS = ['all', 'purchased', 'activated', 'deactivated']
+
+export default function CourseFilter({ onSearchSubmit, onFilterSelect }) {
+  const [searchText, setSearchText] = useState('')
+
   return (
     <div className="flex flex-col md:flex-row items-center my-4">
       <div className="flex mr-2 relative rounded-md">
         <input
+          onChange={({ target: { value } }) => {
+            setSearchText(value)
+          }}
+          value={searchText}
           type="text"
-          name="account"
-          id="account"
+          name="courseHash"
+          id="courseHash"
           className="w-52 xs:w-32 sm:w-96 focus:ring-indigo-500 shadow-md focus:border-indigo-500 block pl-7 p-4 sm:text-sm border-gray-300 rounded-md"
           placeholder="0x2341ab..."
         />
-        <Button>Search</Button>
+        <Button onClick={() => onSearchSubmit(searchText)}>Search</Button>
       </div>
       <div className="relative text-gray-700">
         <select
+          onChange={({ target: { value } }) => {
+            onFilterSelect(value)
+          }}
           className="w-72 h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline"
           placeholder="Regular input"
         >
-          <option>A regular sized select input</option>
-          <option>Another option</option>
-          <option>And one more</option>
+          {OPTIONS.map((option) => (
+            <option key={option} value={option}>
+              {option.charAt(0).toUpperCase() + option.substring(1)}
+            </option>
+          ))}
         </select>
         <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
           <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
